1bfb64ec9a68e6f53aaa8153a5b13d1b59a57edb
Max Gordon <max@gforge.se>
2018-09-23 22:57:45 +0200

    Updates

--- demo.614fa3c0.js
+++ demo.614fa3c0.js
@@ -52353,7 +52353,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '33721' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '38331' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
